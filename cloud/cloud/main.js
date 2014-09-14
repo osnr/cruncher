2ca53abb3066d60f1daa57e33719d344c8c7adb1
Omar Rizwan <omar.rizwan@gmail.com>
2014-09-14 14:38:00 -0700

    parse cloud code to support access control on published docs

---
+++ cloud/cloud/main.js
@@ -0,0 +1,66 @@
+Parse.Cloud.define("publish", function(request, response) {
+    var saveId = request.params.saveId;
+    var data = request.params.data;
+
+    Parse.Cloud.useMasterKey();
+
+    var query = new Parse.Query("Doc");
+    var doc;
+    query.get(saveId).then(function(as) {
+        doc = as; // FIXME is this good practice?
+
+        var publishId = doc.get("publishId");
+
+        var publishQuery = new Parse.Query("PublishDoc");
+        if (publishId) {
+            return publishQuery.get(publishId);
+        } else {
+            return new Parse.Object("PublishDoc");
+        }
+    }).then(function(publishDoc) {
+        // check if whether -- if this publishDoc already existed --
+        // it's actually keyed to this save; if it's not, the user
+        // shouldn't be able to update it
+        var publishSaveId = publishDoc.get("saveId");
+
+        if (!publishSaveId || publishSaveId === saveId) {
+            return Parse.Promise.as(publishDoc);
+        } else {
+            return Parse.Promise.error("Not authorized to publish here.");
+        }
+    }).then(function(publishDoc) {
+        publishDoc.set("saveId", saveId);
+        publishDoc.set("data", data);
+
+        return publishDoc.save();
+
+    }).then(function(publishDoc) {
+        doc.set("publishId", publishDoc.id);
+        doc.set("data", data);
+
+        return doc.save();
+
+    }).then(function(doc) {
+        response.success({ publishId: doc.get("publishId") });
+
+    }, function(error) {
+        response.error(error);
+    });
+});
+
+Parse.Cloud.define("getPublish", function(request, response) {
+    var publishId = request.params.publishId;
+
+    var query = new Parse.Query("PublishDoc");
+    query.get(publishId, {
+        useMasterKey: true,
+
+        success: function(publishDoc) {
+            response.success({ data: publishDoc.get("data") });
+        },
+        error: function(error) {
+            response.error(error);
+        }
+    });
+});
+
